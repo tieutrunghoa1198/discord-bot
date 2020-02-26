c70a3301100ecb01a05d1c0d60eed8843b5dfb0e
Hoà Tiêu <tieutrunghoa1198@gmail.com>
2020-02-27 00:01:02 +0700

    commit

--- index.js
+++ index.js
@@ -1,23 +1,16 @@
 const Discord = require('discord.js-commando');
 const bot = new Discord.Client();
-const TOKEN = 'NTI0OTIwOTU4MTc2MzI5NzM4.DvvISw.zG1uvjS7gAYNu5y1zZ59b1Ow2bo';
+const TOKEN = 'NTI0OTIwOTU4MTc2MzI5NzM4.XlakMA.l47l23OLde0mVtDSkSv6Xt1mikk';
 var name = ['kien', 'ninh', 'toan to', 'canh', 'canh muc', 'cảnh', 'bua', 'bứa', 'bita'];
 var quest = ['ai đẹp trai nhất hạ long', 'lộc lí', 'hôm nay sinh nhật ai', '75 củ đã vô địch sever chưa', 'tí nữa tao comeback', 'hôm nay ăn gì'];
 var current = new Date();
-const http = require('http');
-
-http.createServer((req, res) => {
-    res.writeHead(200, {
-        'Content-type': 'text/plain'
-    });
-    res.write('Hey');
-    res.end();
-}).listen(4000);
+// let time = 'Bây giờ là: ' + current.getHours() + ':' + current.getMinutes() + ':' + current.getSeconds();
 bot.registry.registerGroup('simple', 'Simple');
 bot.registry.registerGroup('music', 'Music');
 bot.registry.registerCommandsIn(__dirname + '/commands');
 bot.registry.registerDefaults();
-global.servers = [];
+
+global.servers =[];
 
 bot.login(TOKEN);
 
@@ -26,22 +19,28 @@ bot.on('ready', () => {
 });
 
 bot.on('message', (message) => {
-    for (let index = 0; index < name.length; index++) {
-        if (message.content == name[index]) {
+    for (let index = 0; index < name.length; index++) 
+    {
+        if(message.content == name[index])
+        {
             message.channel.sendMessage('Địt con mẹ ' + name[index]);
             break;
         }
     }
-    if (message.content == quest[1]) {
+    if(message.content == quest[1])
+    {
         message.channel.sendMessage('chào chú công an');
     }
-    if (message.content == quest[2]) {
+    if(message.content == quest[2])
+    {
         message.channel.sendMessage('Hôm nay sinh nhật chú công an!!!');
     }
-    if (message.content == quest[3]) {
+    if(message.content == quest[3])
+    {
         message.channel.sendMessage('vô địch thiên hạ');
     }
-    if (message.content == quest[4]) {
+    if(message.content == quest[4])
+    {
         message.channel.sendMessage('oke');
     }
 });
