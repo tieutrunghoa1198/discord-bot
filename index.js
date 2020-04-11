6d459df2a8cbfad7e9e46fe75a6af2944c1831c9
Hoà Tiêu <tieutrunghoa1198@gmail.com>
2020-04-11 20:24:39 +0700

    commit

--- index.js
+++ index.js
@@ -16,11 +16,9 @@ BOT.once('ready', () => {
 
 BOT.on('message', msg => {
     if (msg.author.BOT) return
-    if (msg.content.toLowerCase() === "hello") msg.channel.send(` Em có sai với ai đi nữa, có làm cái gì đi nữa, nếu có phải trả giá thì em cũng xin chấp nhận. Bởi vì anh biết đấy, ra xã hội làm ăn bươn chải, liều thì ăn nhiều, không liều thì ăn ít. Muốn thành công thì phải chấp nhận chịu qua đắng cay ngọt bùi. Làm ăn muốn kiếm được tiền phải chấp nhận mạo hiểm, nguy hiểm một tý, nhưng trong tầm kiểm soát. Xã hội này chỉ có làm, chịu khó, cần cù thì bù siêng năng. Chỉ có làm thì mới có ăn. Những cái loại không làm mà đòi có ăn thì chỉ có ăn đầu buồi, ăn cứt.`)
+    if (msg.content.toLowerCase() === "hello") msg.channel.send(`Lô con cặc.`)
     // BOT.commands
 })
-
-
 
 //Login the BOT with provided token in config file 
 BOT.login(TOKEN).catch(err => {
