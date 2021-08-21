503160f132237e8bfc3cd1aa8fe7a859069ad7d3
Hoà Tiêu <tieutrunghoa1198@gmail.com>
2021-08-21 22:55:51 +0700

    update v13

--- index.js
+++ index.js
@@ -1,40 +1,74 @@
 require('dotenv').config();
-const { Client, Intents, Collection } = require('discord.js');
-const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
+const { REST } = require('@discordjs/rest');
+const { Routes } = require('discord-api-types/v9');
 const TOKEN = process.env.TOKEN;
 const fs = require('fs');
+const { Client, Intents } = require('discord.js');
+const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] });
+const commands = [];
 const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
-client.commands = new Collection();
 
-for(const file of commandFiles) {
+client.once('ready', () => {
+	console.log(client.user.username);
+});
+
+client.on('messageCreate', msg => {
+	console.log(msg);
+});
+
+// commands for MEE6 bot 
+client.on('messageCreate', msg => {
+	const channel = '878119742228037744';
+	const botID = '159985870458322944';
+    if(msg.channelId == channel) {
+        if(msg.author.id === botID) {
+            return;
+        }
+				else {
+            msg.delete(1000);
+        }    
+    }
+});
+
+// commands for Groovie bot 
+client.on('messageCreate', msg => {
+	const channel = '878130330068996097';
+	const botID = '234395307759108106';
+    if(msg.channelId == channel) {
+        if(msg.author.id === botID) {
+            return;
+        }
+				else {
+            msg.delete(1000);
+        }    
+    }
+});
+
+// Place your client and guild ids here
+const clientId = process.env.clientId;
+
+for (const file of commandFiles) {
 	const command = require(`./commands/${file}`);
-	client.commands.set(command.data.name, command);
+	commands.push(command.data.toJSON());
 }
 
-// this event will only trigger one time after logging in
-client.once('ready', () => {
-	console.log('Ready!');
-});
+const rest = new REST({ version: '9' }).setToken(TOKEN);
 
-client.on('interactionCreate', async interaction => {
-	if (!interaction.isCommand()) return;
+(async () => {
+	try {
+		console.log('Started refreshing application (/) commands.');
 
-	const { commandName } = interaction;
+		await rest.put(
+			Routes.applicationGuildCommands(clientId, '325650252386271238'),
+			{ body: commands },
+		);
 
-	if (commandName === 'ping') {
-		await interaction.reply('Pong!');
+		console.log('Successfully reloaded application (/) commands.');
 	}
-	else if (commandName === 'beep') {
-		await interaction.reply('Boop!');
+	catch (error) {
+		console.error(error);
 	}
-	else if (commandName === 'server') {
-		await interaction.reply(`This server's name is: ${interaction.guild.name}`);
-	}
-});
-
-client.on('message', msg => {
-	console.log(msg.author);
-});
+})();
 
 // login
 client.login(TOKEN);
