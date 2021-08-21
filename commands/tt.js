3d50e38e376cda267067c9e61626a8769d3a7de1
Hoà Tiêu <tieutrunghoa1198@gmail.com>
2021-08-22 00:47:23 +0700

    update commands

--- commands/tt.js
+++ commands/tt.js
@@ -2,9 +2,9 @@ const { SlashCommandBuilder } = require('@discordjs/builders');
 
 module.exports = {
   data: new SlashCommandBuilder()
-    .setName('info')
+    .setName('tt')
     .setDescription('Your ID!'),
   async execute(interaction) {
-      await interaction.reply('Your ID: ' + interaction.user.id);
+      await interaction.reply('ID của nhà mình là ' + interaction.user.id + ' nha!');
     },
 };
