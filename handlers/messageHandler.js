ccd469a1a740086867b223f2eb2894d4ea1e0065
Hoà Tiêu <tieutrunghoa1198@gmail.com>
2021-08-26 23:35:43 +0700

    update handlers

---
+++ handlers/messageHandler.js
@@ -0,0 +1,25 @@
+const channels = require('../models/Channel.js');
+
+const messageHandler = msg => {
+  const id = msg.channelId;
+  const authorId = msg.author.id;
+
+  channels.find({ channelId: id }, function(err, channel) {
+    // if channel not found => do nothing
+    if (channel.length === 0) {
+      return;
+    }
+
+    // if channel is found, then search for allowed users by authorId and channelId.
+    channels.find({ channelId: id, permittedUsers: authorId }, function(err, data) {
+      if (err) {
+        console.log(err);
+      } 
+      else if (data.length === 0) {
+        msg.delete();
+      }
+    });
+  });
+};
+
+module.exports = { messageHandler };
--- index.js
+++ index.js
@@ -1,13 +1,17 @@
 require('dotenv').config();
 const TOKEN = process.env.TOKEN;
 const uri = process.env.uri;
-const channels = require('./models/Channel.js');
-const fs = require('fs');
+
+const { messageHandler } = require('./handlers/messageHandler.js');
+const { commandHandler } = require('./handlers/commandHandler.js');
+
 const { Client, Intents, Collection } = require('discord.js');
 const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] });
-const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
-const mongoose = require('mongoose');
 
+const fs = require('fs');
+const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
+
+const mongoose = require('mongoose');
 mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 mongoose.connection.on('error', err => {
   console.log(err);
@@ -15,29 +19,14 @@ mongoose.connection.on('error', err => {
 
 client.commands = new Collection();
 
-// load command files 
 for (const file of commandFiles) {
 	const command = require(`./commands/${file}`);
-	// set a new item in the Collection
-	// with the key as the command name and the value as the exported module
 	client.commands.set(command.data.name, command);
 }
 
-// this code to handle interaction
+// commands handler
 client.on('interactionCreate', async interaction => {
-	if (!interaction.isCommand()) return;
-
-	const command = client.commands.get(interaction.commandName);
-
-	if (!command) return;
-
-	try {
-		await command.execute(interaction);
-	} 
-	catch (error) {
-		console.error(error);
-		await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
-	}
+	commandHandler(interaction, client);
 });
 
 // bot is ready to use!!
@@ -48,26 +37,7 @@ client.once('ready', () => {
 
 // delete message in specified channels
 client.on('messageCreate', msg => {
-	const id = msg.channelId;
-	const authorId = msg.author.id;
-
-	channels.find({ channelId: id }, function(err, channel) {
-		// if channel not found => do nothing
-		if(channel.length === 0) {
-			return;
-		}
-		
-		// if channel is found, then search for allowed users by authorId and channelId.
-		channels.find({ channelId: id, permittedUsers : authorId }, function(err, data) {
-			if(err) {
-				console.log(err);
-			}
-			else if(data.length === 0) {
-				msg.delete();
-			}
-		});
-		
-	});
+	messageHandler(msg);
 });
 
 // login
