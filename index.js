2830f739e67d14c2845ebe2a073923e996b33402
Hoà Tiêu <tieutrunghoa1198@gmail.com>
2021-08-26 22:20:51 +0700

    fixed load new commands

--- index.js
+++ index.js
@@ -1,22 +1,19 @@
 require('dotenv').config();
 const TOKEN = process.env.TOKEN;
 const uri = process.env.uri;
-const loadCommands = require('./registerCommand.js');
 const channels = require('./models/Channel.js');
 const fs = require('fs');
-const mongoose = require('mongoose');
+
 const { Client, Intents, Collection } = require('discord.js');
 const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] });
 const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
-
+const mongoose = require('mongoose');
 mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 mongoose.connection.on('error', err => {
   console.log(err);
 });
 
 client.commands = new Collection();
-
-loadCommands.test12();
 
 // load command files 
 for (const file of commandFiles) {
@@ -49,16 +46,18 @@ client.once('ready', () => {
 	console.log(client.user.username);
 });
 
-// delete not neccessary message 
+// delete message in specified channels
 client.on('messageCreate', msg => {
 	const id = msg.channelId;
 	const authorId = msg.author.id;
 
 	channels.find({ channelId: id }, function(err, channel) {
+		// if channel not found => do nothing
 		if(channel.length === 0) {
 			return;
 		}
 		
+		// if channel is found, then search for allowed users by authorId and channelId.
 		channels.find({ channelId: id, permittedUsers : authorId }, function(err, data) {
 			if(err) {
 				console.log(err);
