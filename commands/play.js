1920c010d11beb4873356acdc4bccf42633fd997
Hoà Tiêu <tieutrunghoa1198@gmail.com>
2021-09-03 17:39:12 +0700

    test cookie

--- commands/play.js
+++ commands/play.js
@@ -1,3 +1,4 @@
+require('dotenv').config();
 const { SlashCommandBuilder } = require('@discordjs/builders');
 const ytdl = require('ytdl-core');
 const { joinVoiceChannel, StreamType, createAudioResource } = require('@discordjs/voice');
@@ -9,8 +10,19 @@ async function initiateMusic(interaction, client) {
   const voiceChannel = member.voice.channel;
   const player = client.player;
   const link = interaction.options.getString('link');
-  const info = await ytdl.getInfo(link);
+  const info = await ytdl.getInfo(
+    link, 
+    {
+      requestOptions: {
+        headers: {
+          cookie: process.env.COOKIE,
+          'x-youtube-identity-token': process.env.YTTK,
+        },
+      },
+    },
+    );
   const format = ytdl.chooseFormat(info.formats, { quality: 'highestaudio' });
+  // QUFFLUhqbnBSVXN2S1BmVjZvN0lRTXNYR1Y5U1VOMnJ2UXw=
   // if users have not connected to a voice channel yet, tell em
   if(await !voiceChannel) {
     await interaction.reply('Please connect to a voice channel.');
@@ -18,9 +30,9 @@ async function initiateMusic(interaction, client) {
   }
 
   const connection = joinVoiceChannel({
-      channelId: voiceChannel.id,
-      guildId: interaction.guildId,
-      adapterCreator: guild.voiceAdapterCreator,
+    channelId: voiceChannel.id,
+    guildId: interaction.guildId,
+    adapterCreator: guild.voiceAdapterCreator,
   });
 
   const resource = createAudioResource(format.url, { inputType: StreamType.Arbitrary });
@@ -36,58 +48,12 @@ module.exports = {
     .addStringOption(option => option.setName('link').setDescription('Enter a link')),
   async execute(interaction, client) {
     const link = interaction.options.getString('link');
-
     // if that if the input data is a accepted url
     if(!ytdl.validateURL(link)) {
       await interaction.reply('Please enter a youtube link or a search term.');
       return;
     }
-
     // get channel id and voice channel if connected
     await initiateMusic(interaction, client);
-    
-
-    /*
-        keep doing with unpause function 
-        then create a queue to play the next song in current queue
-    */ 
-  
-    // then play a list using a link
-    
-    // const guild = client.guilds.cache.get(interaction.guildId);
-    // const member = guild.members.cache.get(interaction.user.id);
-    // const voiceChannel = member.voice.channel;
-    // // console.log(voiceChannel);
-
-    // const info = await ytdl.getInfo('https://www.youtube.com/watch?v=4POUDVKNXVI&list=RDMM4POUDVKNXVI&start_radio=1');
-    // const format = ytdl.chooseFormat(info.formats, { quality: 'highestaudio' });
-    // console.log('Format found!', format);
-
-    // const stream = ytdl(link, { quality:'highestaudio', filter:'audioonly' });
-    // const resource = createAudioResource(stream, { inputType: StreamType.Arbitrary });
-    
-    // const player = createAudioPlayer({
-    //     behaviors: {
-    //         noSubscriber: NoSubscriberBehavior.Play,
-    //     },
-    // });
-    // // console.log(player);
-    // const connection = joinVoiceChannel({
-    //     channelId: voiceChannel.id,
-    //     guildId: interaction.guildId,
-    //     adapterCreator: guild.voiceAdapterCreator,
-    // });
-    
-    // connection.subscribe(player);
-    // player.play(resource);
-    // // console.log(player);
-    // // player.onReadableCallback(data => console.log(data));
-    // // player.
-    // // console.log(player);
-    // player.on('stateChange', (oldState, newState) => {
-    //     console.log(`Audio player transitioned from ${oldState.status} to ${newState}`);
-    // });
-    // console.log('Format found!', format);
-      
     },
 };
--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const { createAudioPlayer } = require('@discordjs/voice');
 const mongoose = require('mongoose');
 const entity = require('./entities/entity.js');
 const handler = require('./handlers/index.js');
+const player = createAudioPlayer();
 const client = new Client(
 	{ 
 		intents: 
@@ -14,13 +15,13 @@ const client = new Client(
 			Intents.FLAGS.GUILD_VOICE_STATES,
 		],
 	});
-// create player, connection, a queue
-const player = createAudioPlayer();
+
 // initiate db and commands 
 entity.mongodb.dbConnect(mongoose);
 entity.commands.load(client);
 client.player = player;
 
+// listen to state change
 player.on('stateChange', (oldState, newState) => {
 	console.log(`Audio player transitioned from ${oldState.status} to ${newState.status}`);
 });
