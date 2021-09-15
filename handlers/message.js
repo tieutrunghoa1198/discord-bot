56f807c8efe55f5710313e05e796d35acf0e92cf
Hoà Tiêu <tieutrunghoa1198@gmail.com>
2021-09-15 12:56:41 +0700

    update data not found bug in message.js

--- handlers/message.js
+++ handlers/message.js
@@ -2,7 +2,14 @@ const channels = require('../models/Channel.js');
 
 const message = async (msg) => {
   const id = msg.channelId;
+  
   channels.find({ channelId: id }, async function(err, data) {
+    // if there is no data for filtering
+    if(data.length === 0) {
+      return;
+    }
+
+    // if permitted users are in database, then pass them
     if(msg.member.roles.cache.get(data[0].permittedUsers[0])) {
       return;
     }
