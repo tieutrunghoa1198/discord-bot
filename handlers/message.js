2b9bac8ba3a42ae4df23dec0ae8b05baa9056036
tieu hoa <tieutrunghoa1198@gmail.com>
2022-08-22 16:59:31 +0700

    final fix

--- handlers/message.js
+++ handlers/message.js
@@ -3,19 +3,15 @@ const messageControl = require('../controller/messageControl.js');
 
 const message = async (msg, client) => {
   const { content } = msg;
-  // # thóc-king
-  const AKG_SV_MAIN_CHAT = '325650252386271238';
+  // # miu-sic-que
+  const AKG_SV_MAIN_CHAT = '878130330068996097';
 
   if (msg.channelId === AKG_SV_MAIN_CHAT) {
     return;
   }
   // check permission before execute
-  if(content.startsWith('https://www.youtube.com')) {
-    if (msg.channelId === AKG_SV_MAIN_CHAT) {
-      return;
-    } else {
-      await music.main(msg, client);
-    }
+  if(content.startsWith('https://www.youtube.com') && (msg.channelId === AKG_SV_MAIN_CHAT)) {
+    await music.main(msg, client);
   }
 
   await messageControl.deleteMsg(msg);
