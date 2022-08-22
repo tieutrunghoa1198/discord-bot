8fe185bf4047c59e6bf9feaa3836832921635b5d
tieu hoa <tieutrunghoa1198@gmail.com>
2022-08-22 17:02:01 +0700

    final fix

--- handlers/message.js
+++ handlers/message.js
@@ -6,9 +6,6 @@ const message = async (msg, client) => {
   // # miu-sic-que
   const AKG_SV_MAIN_CHAT = '878130330068996097';
 
-  if (msg.channelId === AKG_SV_MAIN_CHAT) {
-    return;
-  }
   // check permission before execute
   if(content.startsWith('https://www.youtube.com') && (msg.channelId === AKG_SV_MAIN_CHAT)) {
     await music.main(msg, client);
